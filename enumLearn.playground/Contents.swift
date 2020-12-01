e310c10517e3624742f7866b94c1bf4514181ef4
ThangLai <ronaldocr9803@gmail.com>
2020-12-01 14:38:31 +0700

    First commit

---
+++ enumLearn.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+enum Rank: Int {
+    case ace = 1
+    case two, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king
+
+    func simpleDescription() -> String {
+        switch self {
+        case .ace:
+            return "ace"
+        case .jack:
+            return "jack"
+        case .queen:
+            return "queen"
+        case .king:
+            return "king"
+        default:
+            return String(self.rawValue)
+        }
+    }
+}
+let ace = Rank.ace
+let aceRawValue = ace.rawValue
+let two = Rank.two
+if let convertedRank = Rank(rawValue: 2) {
+    let threeDescription = convertedRank.simpleDescription()
+    print(threeDescription)
+}
+
